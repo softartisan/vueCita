0bfe7fe7938c680b7f92cb5b3f4797cb930cfd5d
softartisan <slcaniob@gmail.com>
2019-07-26 21:36:13 -0400

    p c

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <Header :quoteCount="quotes.length" :maxQuotes="maxQuotes"/>
+        <NewQuote @quoteAdded="newQuote"/>
+        <QuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"/>
+        <div class="row">
+            <div class="col-sm-12 text-center mt-5">
+                <div class="alert alert-info">
+                    Información: Haz click en una Cita para eliminarla.
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+
+    export default {
+        data: () => ({
+            quotes: [
+                'Prueba'
+            ],
+            maxQuotes: 10
+        }),
+        methods: {
+            newQuote(quote) {
+                if(this.quotes.length >= this.maxQuotes){
+                    alert('Borre una Quote primero.');
+                }else{
+                    this.quotes.push(quote);
+                }
+                
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
+            }
+        },
+        components: {
+            QuoteGrid,
+            NewQuote,
+            Header
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row">
+    <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+      <p>Nueva Cita</p>
+      <textarea class="form-cntrol" rows="3" v-model="quote"></textarea>
+    </div>
+    <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+      <button class="btn btn-primary" @click.prevent="createNew">Agregar Cita</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    quote: ''
+  }),
+  methods: {
+    createNew() {
+      this.$emit('quoteAdded', this.quote);
+      this.quote = '';
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
